ae5cda5cb7e352527b258e20f616b0c6d742b87b
Link <mnchung3@gmail.com>
2021-11-20 23:19:25 +0900

    공통 에러 예외처리 변경

--- src/main/java/org/link/camel/config/error/ControllerExceptionHandler.java
+++ src/main/java/org/link/camel/config/error/ControllerExceptionHandler.java
@@ -4,33 +4,55 @@ import lombok.extern.slf4j.Slf4j;
 import org.link.camel.config.error.exception.ServiceLogicException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.security.InvalidParameterException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("handleMethodArgumentNotValidException", e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR, e.getBindingResult());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BindException.class)
+    protected ResponseEntity<ErrorResponse> handleBindException(BindException e) {
+        log.error("handleBindException", e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR, e.getBindingResult());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        log.error("handleMethodArgumentTypeMismatchException", e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
         log.error("handleHttpRequestMethodNotSupportedException", e);
-        ErrorResponse response = new ErrorResponse();
-
+        ErrorResponse response = ErrorResponse.of(ErrorCode.METHOD_NOT_ALLOWED);
         return new ResponseEntity<>(response, HttpStatus.METHOD_NOT_ALLOWED);
     }
 
-    @ExceptionHandler(InvalidParameterException.class)
-    protected ResponseEntity<ErrorResponse> handleInvalidParameterException(InvalidParameterException e) {
-        log.error("handleInvalidParameterException", e);
-        ErrorResponse response = new ErrorResponse();
+      // 인증 관련 오류 추가 필요!
+//    @ExceptionHandler(AccessDeniedException.class)
+//    protected ResponseEntity<ErrorResponse> handleInvalidParameterException(InvalidParameterException e) {
+//        log.error("handleInvalidParameterException", e);
+//        ErrorResponse response = new ErrorResponse();
+//
+//        return new ResponseEntity<>(response, HttpStatus.resolve(500));
+//    }
 
-        return new ResponseEntity<>(response, HttpStatus.resolve(500));
-    }
-
-    // RuntimeException
+    // 비즈니스 로직 에러
     @ExceptionHandler(ServiceLogicException.class)
     protected ResponseEntity<ErrorResponse> handleServiceLogicException(ServiceLogicException e) {
         log.error("handleServiceLogicException", e);
@@ -45,7 +67,7 @@ public class ControllerExceptionHandler {
     protected ResponseEntity<ErrorResponse> handleException(Exception e) {
         log.error("handleException", e);
 
-        ErrorResponse response = ErrorResponse.of(ErrorCode.ERROR);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
